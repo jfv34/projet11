1f50b510bbda61a94135c707a8bb05944c94f4b1
JFV34 <jfr.vincler@live.fr>
2020-10-06 19:49:38 +0200

    connect by google or mail

---
+++ app/src/main/java/com/vincler/jf/projet11/ui/main/MainActivity.java
@@ -0,0 +1,47 @@
+package com.vincler.jf.projet11.ui.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.vincler.jf.projet11.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123; // For connect by Firebase
+    private MainActivityViewModel mainActivityViewModel = new MainActivityViewModel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.LoginTheme)
+                        //.setLogo(R.drawable.logo)
+                        .build(),
+                RC_SIGN_IN);
+
+
+    }
+}
---
+++ app/src/main/java/com/vincler/jf/projet11/ui/main/MainActivityViewModel.java
@@ -0,0 +1,10 @@
+package com.vincler.jf.projet11.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+class MainActivityViewModel extends ViewModel {
+
+    private static final int RC_SIGN_IN = 123; // For connect by Firebase
+
+
+}
