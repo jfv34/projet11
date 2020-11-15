b23632b6f44b54716fc54054a59946b854371f7f
JFV34 <jfr.vincler@live.fr>
2020-11-15 10:28:11 +0100

    fixed data for SPANISH

--- app/src/main/java/com/vincler/jf/projet11/presentation/menu/MenuActivity.java
+++ app/src/main/java/com/vincler/jf/projet11/presentation/menu/MenuActivity.java
@@ -9,6 +9,7 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.vincler.jf.projet11.R;
+import com.vincler.jf.projet11.api.InsertInitialData;
 import com.vincler.jf.projet11.utils.Utils;
 
 import java.util.Arrays;
